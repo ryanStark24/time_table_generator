f52f8e988f7a8380317561a5b32cde1d25ff5845
ryanstark24 <anshul2411.rs@gmail.com>
2018-04-10 11:47:45 +0530

    fixed

--- API/routes/timetable.js
+++ API/routes/timetable.js
@@ -14,10 +14,11 @@ router.post('/', checkAuth, (req, res, next) => {
   // let data = new Data();
 
   let final_table = new tester(req.body);
-  // res.status(200).json({
-  //   timetable: final_table
-  // });
-  res.render('timetable', { title: 'TimeTable', timetable: final_table });
+  console.log(final_table)
+  res.status(200).json({
+    timetable:final_table.Sections
+  });
+  // res.render('timetable', { title: 'TimeTable', timetable: final_table });
 });
 
 module.exports = router;
